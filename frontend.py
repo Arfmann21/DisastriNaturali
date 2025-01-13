01cb48d6da54c25834777dea4fe33450f58f6b99
Arfmann21 <arfmann@protonmail.com>
2025-01-13 17:33:25 +0100

    Aggiunto il frontend con streamlit

---
+++ frontend.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from query_backend import query
+
+st.write("""
+# My first app
+Hello *world!*
+""")
+
+if st.button("Mostra utenti verificati"):
+    st.write("Risultati della query:")
+    
+    # Esegui la query su PySpark
+    result = query()
+    
+    # Mostra i risultati
+    for row in result:
+        st.write(row)
