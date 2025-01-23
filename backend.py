f465afc8199e8d2d6409954d8aed31556d7b573e
Arfmann21 <arfmann@protonmail.com>
2025-01-23 15:19:13 +0100

    Funzione query e comandi aggioarni

--- backend.py
+++ backend.py
@@ -4,7 +4,20 @@ spark = SparkSession.builder.master("spark://192.168.1.13:7077") \
     .appName("QueryBackend") \
     .getOrCreate()
 
-df = spark.read.json("output").createOrReplaceTempView("Test")
+df = spark.read.json("output").createOrReplaceTempView("Dataset")
 
-def query():
-    return spark.sql("SELECT name FROM Test WHERE verified = true").collect()
+def query(label = None, username = None, verified = None, created_at = None, possibly_sensitive = None, place = None, coordinates = None, groupByValue = None):
+    #return spark.sql("SELECT * FROM Dataset").collect()
+
+    query = "SELECT * FROM Dataset WHERE true"
+
+    if(verified is not None): query += "AND verified = " + verified
+    if(created_at is not None): query += " AND createdAt = " + created_at
+    if(possibly_sensitive is not None): query += " AND possibly_sensitive = " + possibly_sensitive
+    if(place is not None): query += " AND place = " + place
+    if(coordinates is not None): query += " AND coordinates = " + coordinates
+    if(label is not None):
+       query += " AND aidr_label = \"" + label[0] + "\""
+       
+    if(groupByValue is not None): query += " GROUPBY " + groupByValue
+    return spark.sql(query).collect()
--- frontend.py
+++ frontend.py
@@ -1,16 +1,24 @@
 import streamlit as st
 from backend import query
 
-st.write("""
-# My first app
-Hello *world!*
+st.title("Disastri Naturali")
+st.subheader("""
+Analisi di dati per l'uragano Harvey
 """)
 
+
+basic_filters, advancedButton, runQuery = st.columns([0.7, 0.3, 0.3])
+
+basic_filters_container = basic_filters.container(border=True) 
+basic_filters_container
+basic_filters_container.markdown("Test")
+advancedButton.markdown("Test 2")
+runQuery.markdown("Test3")
 if st.button("Mostra utenti verificati"):
     st.write("Risultati della query:")
     
     # Esegui la query su PySpark
-    result = query()
+    result = query(label=['relevant_information'])
     
     # Mostra i risultati
     for row in result:
