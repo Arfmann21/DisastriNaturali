cb9ec8f8d0cff60dfba82ddc034cd78f743c59a9
Arfmann21 <arfmann@protonmail.com>
2025-01-24 22:01:28 +0100

    Impostazione parziale layout

--- frontend.py
+++ frontend.py
@@ -1,25 +1,29 @@
 import streamlit as st
-from backend import query
+
+st.set_page_config(layout="wide")
 
 st.title("Disastri Naturali")
 st.subheader("""
 Analisi di dati per l'uragano Harvey
 """)
 
+# Aggiunge uno spazio vuoto
+st.markdown("#")
 
-basic_filters, advancedButton, runQuery = st.columns([0.7, 0.3, 0.3])
+# Colonne per i filtri di base, il pulsante per aprire i filtri avanzati e quello per eseguire la query
+basic_filters_col, advancedButton, runQuery = st.columns([0.7, 0.3, 0.3])
 
-basic_filters_container = basic_filters.container(border=True) 
-basic_filters_container
-basic_filters_container.markdown("Test")
+with basic_filters_col:
+    # Colonne per i filtri base, una per ciascuno
+    label, verified, created_at, test = st.columns([0.2, 0.1, 0.3, 0.3])
+
+    with label:
+        label_label = st.markdown("##### Label")
+        st.multiselect("Collapsed",["Test1", "Test2"], key ="TEST", label_visibility="collapsed")
+
+    with verified:
+        verified_label = st.markdown("##### Verified")
+        st.selectbox("Collapsed", ["Tutti", "True", "False"], label_visibility="collapsed")
+
 advancedButton.markdown("Test 2")
 runQuery.markdown("Test3")
-if st.button("Mostra utenti verificati"):
-    st.write("Risultati della query:")
-    
-    # Esegui la query su PySpark
-    result = query(label=['relevant_information'])
-    
-    # Mostra i risultati
-    for row in result:
-        st.write(row)
